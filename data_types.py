4fb42f84f27ebd646c0f6087223926860d99fd00
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-01-02 17:17:41 +0600

    first commit

---
+++ data_types.py
@@ -0,0 +1,15 @@
+print("Hello world!");
+print("My favorite drink is Earl Grey tea.");
+print("¯\_(ツ)_/¯");
+print("""Hello, World!
+      It's great to be here!""");
+type("Andrew");
+type("""
+Numbers, text, and truth,
+Strings, ints, and floats in our code,
+Data shapes our path
+""");
+type(2.99);
+print(28+35+43+50+65+70+68+66+75+80+95);
+print(1.05**10);
+print((75 - 32) * 5 / 9);
