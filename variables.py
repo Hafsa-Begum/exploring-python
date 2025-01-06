52bf356da27822258151b3fc22ab848e22a01497
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-01-06 18:09:35 +0600

    practice variables problem

--- variables.py
+++ variables.py
@@ -43,3 +43,20 @@ name = "Otto Matic"
 print(f"""{name}'s dog age is {dog_age}. So a dog that's about
 {dog_age} would be the same age as {name}. Any dog born about {dog_age}
 years ago would be in the same stage of life as {name}.""")
+# some practices
+# Create a variable called 'my_name' and assign it the value of your name as a string.
+# Then print out a greeting using the variable, like "Hello, Andrew!"
+my_name = 'Andrew'
+print(f"Hello, {my_name}")
+# Enter your favorite number below and store it in a variable called 'fav_num'. 
+# Print out a message telling you what your favorite number plus 10 is.
+fav_num = 11
+fav_num = fav_num + 10
+print(f"Your favorite number plus 10 is {fav_num}")
+# Create two variables, 'countries_visited' and 'countries_to_visit' and assign them the number of
+# countries you've been to and the number of countries you hope to visit. Then complete the print statement.
+countries_visited = 1
+countries_to_visit= 5
+countries_visited = countries_visited + countries_to_visit
+print(f"""I have visited {countries_visited} countries. I plan to visit {countries_to_visit} more countries, 
+      and when I'm done I will have visited {countries_visited} countries.""")
