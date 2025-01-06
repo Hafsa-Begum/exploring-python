756cff036efc880e0eaa6c72ea4199b6c5cc620f
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-01-06 17:42:02 +0600

    explore variables basic

---
+++ README.md
@@ -0,0 +1,11 @@
+# Variables
+### Characteristics
+- variables store value which assign to it
+- variables name should be descriptive and meaningful
+- variable name must not contain space
+- it can't start having numbers , spaces
+- variable can hold integers, floating-point, string and more data
+- it can be mutable(changable->list) and immutable(unchangable->integers, float, string)
+- variable's scope determines where it can be accessed from in the code
+- variable's lifetime is the period during which it exists in memory, usually from when it's created until the function ends or the program exits
+- Variables allow you to code efficiently!
---
+++ variables.py
@@ -0,0 +1,45 @@
+age = 28
+print(f"Age: {age}")
+age = 5
+print(f"Age: {age}")
+name = "Otto"
+gnome_height = 12.7
+print(f"Name: {name}")
+print(f"Gnome height: {gnome_height}")
+
+
+# characteristics variable
+# Variables are case-sensitive
+# Variables help you store values that constantly change
+score = 0 # now score is 0
+print(score)
+score = score + 50 # now score is 0 + 50 which is 50
+print(score)
+score = score + 100 # now score is 100 + 50 which is 150
+print(score)
+score = score + 300 # now score is 150 + 300 which is 450
+print(score)
+print(f"Your final score was: {score}") # prints 450
+#Variables allow you to code efficiently!
+# 
+#  ###
+print(49 / 7)
+print(f"Otto's age in dog years is {49 / 7}")
+dog_age = 49 / 7
+print(f"""Otto's dog age is {49/7}. So a dog that's about
+{49/7} would be the same age as Otto. Any dog born about {49/7}
+years ago would be in the same stage of life as Otto.""")
+print(f"""Otto's dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as Otto. Any dog born about {dog_age}
+years ago would be in the same stage of life as Otto.""")
+dog_age = 50/7
+print(f"""Otto's dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as Otto. Any dog born about {dog_age}
+years ago would be in the same stage of life as Otto.""")
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as {name}. Any dog born about {dog_age}
+years ago would be in the same stage of life as {name}.""")
+name = "Otto Matic"
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as {name}. Any dog born about {dog_age}
+years ago would be in the same stage of life as {name}.""")
